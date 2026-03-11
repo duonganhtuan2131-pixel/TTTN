0a8eca2671e7635046409f7a0b4eaccd67899ce8
duonganhtuan2131-pixel <duonganhtuan2131@gmail.com>
2026-03-11 12:47:51 +0700

    update project

---
+++ server/models/userModel.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    full_name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    password: { type: String, required: true },
+    avatar: { type: String }, // Thêm trường avatar
+    role: { type: String, default: 'customer' }, // 'customer', 'staff', 'admin', 'hotelOwner'
+}, { timestamps: true });
+
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default userModel;
